6d95d7493c0ab3759777e2ac4668f6c7ea6bef40
Gabriel Rodriguez <gfrodriguezclon@outlook.com>
2023-03-26 18:47:06 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+function Profile() {
+  const name = "Katherine Johnson"
+  return (
+    <>
+    <img
+      src="https://edurank.org/assets/img/alumni/katherine-johnson.jpg"
+      alt={name}
+    />
+      <h4>{name}</h4>
+      <p
+        align="justify"
+        >Katherine Johnson was an American mathematician whose calculations of orbital mechanics as a NASA employee were critical to the success of the first and subsequent U.S. crewed spaceflights. During her 33-year career at NASA and its predecessor, she earned a reputation for mastering complex manual calculations and helped pioneer the use of computers to perform the tasks. The space agency noted her "historical role as one of the first African-American women to work as a NASA scientist".</p>
+  </>
+ );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+    </section>
+  );
+}
